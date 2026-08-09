eae041b52551e1a0a1ca22fab215833b65dc2efb
Himath Nimpura <himathd.23@cse.mrt.ac.lk>
2026-08-09 16:11:18 +0530

    chore: remove legacy site sources

--- Old/src/assets/client.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from "@sanity/client";
-
-export default sanityClient({
-    projectId: 'i32b0q2c',
-    dataset: 'production',
-    useCdn: true,
-    apiVersion: "2021-03-25"
-});
--- Old/src/pages/PageComingSoon.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <main>Coming Soon!</main>
-</template>
-
-<style scoped>
-main {
-    font-size: 2rem;
-}
-</style>
